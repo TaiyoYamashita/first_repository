4d2191585ec597f792df7ec4f7b176ab11b4b701
TaiyoYamashita <sunlight360@icloud.com>
2024-05-05 15:03:40 +0900

    カリキュラム08-6

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -13,9 +13,24 @@
                 <div class='post'>
                     <a href="/posts/{{ $post->id }}"><h2  class='title'>{{ $post->title }}</h2></a>
                     <p class='body'>{{ $post->body }}</p>
+                    <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button type="button" onclick="deletePost({{ $post->id }})">delete</button>
+                    </form>
                 </div>
             @endforeach
         </div>
         <div class='paginate'>{{ $posts->links() }}</div>
+        <!--JavaScriptの呼び出しをする。サイトの処理速度を上げるためにここで呼び出す。ポップアップを出力してOKを選択した場合に削除をする。-->
+        <script>
+            function deletePost(id) {
+                'use strict'
+                
+                if(confirm('削除すると復元できません。\n本当に削除しますか？')) {
+                    document.getElementById(`form_${id}`).submit();
+                }
+            }
+        </script>
     </body>
 </html>
